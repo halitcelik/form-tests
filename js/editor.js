a264a8b30abfa138ec9a642cf71ae5182461563a
Halit Celik <halit@cruncher.ch>
2018-09-13 14:01:27 +0200

    forms got better

--- js/editor.js
+++ js/editor.js
@@ -8,19 +8,26 @@ form.addEventListener('input', function(e){
 	if (e.target.name === 'amount'){
 		const value = parseFloat(e.target.value);
 
+		const topValue = Math.sin((value / 100) * Math.PI)
 
-		thing.style.top = value + '%';
-		thing.style.left = value + '%';
+
+
+		thing.style.top = topValue * 100 + 'vh';
+
+		thing.style.left = value + 'vw';
+
+
 
 		rgb = [
         value,
-        value + 155,
-        value + 100
+        value * 2,
+        value / 2
 		];
 
 		thing.style.width = value/10 + 'rem';
 		thing.style.height = value/10 + 'rem';
-	    body.style.color = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] +');';
+		thing.style.backgroundColor = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ");";
+	    console.log(thing.style.backgroundColor = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")");
 	}
 
 });
